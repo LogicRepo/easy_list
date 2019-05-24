be8b5f52b17c07a34c2bffa40c93fcc583e217ef
avsheksingh <avsheksingh@outlook.com>
2019-05-24 07:42:53 +0530

    Text fields are added

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePage();
+  }
+}
+
+class _ProductCreatePage extends State<ProductCreatePage> {
+  String titleValue;
+  String descriptionValue;
+  double priceValue;
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Create a Product'),);
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              titleValue = value;
+            });
+          },
+        ),
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              descriptionValue = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              priceValue = double.parse(value);
+            });
+          },
+        ),
+      ],
+    );
   }
 }
